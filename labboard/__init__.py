1fc89690f38388037c6e9060aa9aa877ee936246
wbs306 <wbs306@gmail.com>
2022-02-10 17:18:03 +0800

    Add QWeather API

--- labboard/__init__.py
+++ labboard/__init__.py
@@ -5,10 +5,13 @@ from flask import Flask
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+    key_file = open(os.path.join(app.instance_path, 'qweather_key.key'))
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'test.db'),
+        QWEATHER_KEY=key_file.read()
     )
+    key_file.close()
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
