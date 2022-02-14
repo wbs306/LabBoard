19ce4abf20108fac53bfebf73f786fbdf07fd65b
wbs306 <wbs306@gmail.com>
2022-02-14 11:53:51 +0800

    Pass packages state to board template

--- labboard/board.py
+++ labboard/board.py
@@ -31,8 +31,14 @@ def load_board():
     }
     if (record_exist):
         from labboard.weather import get_weather
+        from labboard.express import get_express_state
         with open(current_app.config["RECORD_FILE"], "r") as f:
             record = json.load(f)
             kwargs["weather"] = get_weather(record["city_code"])
             packages = record["packages"]
+        kwargs["packages_info"] = []
+        for i in packages:
+            state = get_express_state(i["number"], i["company"])
+            state["number"] = i["number"]
+            kwargs["packages_info"].append(state)
     return render_template('board.html', **kwargs)
