622a6596e73cb33762d71af2d3d554fff3f0eb9f
wbs306 <wbs306@gmail.com>
2022-02-25 22:04:27 +0800

    Bind deletePackage() to delete btn

---
+++ labboard/static/express_card.js
@@ -0,0 +1,16 @@
+function deletePackage() {
+    let curr_item = $(".package-item.active");
+    let curr_track = $(".track-detail.active");
+    if ($(".package-item").length > 1) {
+        curr_item.next().addClass("active");
+        curr_track.next().addClass("active");
+    }
+
+    curr_item.remove();
+    curr_track.remove();
+
+    $.post($SCRIPT_ROOT + "/express/deletePackage", {
+        "number": curr_item.attr("href").split("-")[1],
+        "name": curr_item.find("h5").text(),
+    });
+}
