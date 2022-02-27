4cf662c481db449fe25c428e02bb478f14262d1c
wbs306 <wbs306@gmail.com>
2022-02-27 11:33:55 +0800

    Implement add package

--- labboard/static/express_card.js
+++ labboard/static/express_card.js
@@ -3,6 +3,56 @@ $(".track:first-child p").removeClass("text-muted");
 $(".track-detail:first-child").addClass("active");
 $(".package-item:first-child").addClass("active");
 
+let input_timer;
+let last_input;
+$("#add-package input").on("keyup", e => {
+    clearTimeout(input_timer);
+    input_timer = setTimeout(() => {
+        let curr_input = $("#add-package input").val();
+        if (last_input == curr_input)
+            return;
+        last_input = curr_input;
+        let spinner = $("<div class='spinner-border text-primary' role='status'><span class='visually-hidden'>Loading...</span></div>");
+        $("#add-package p").remove();
+        $("#add-package .spinner-border").remove();
+        $("#add-package").append(spinner);
+        $.post($SCRIPT_ROOT + "/express/getExpressCompany", {
+            "number": curr_input
+        }, (data) => {
+            spinner.remove();
+            let company = $("<p></P>");
+            if (data["company"]) {
+                company.attr("id", "company-" + data["company"]);
+                $("#add-package button").removeClass("disabled");
+            }                
+            company.text(data["name"]);
+            $("#add-package").append(company);
+        });
+    }, 2000)
+})
+
+function addPackage() {
+    let number = $("#add-package input").val();
+    let company = $("#add-package p").attr("id").split("-")[1];
+    $.post($SCRIPT_ROOT + "/express/addPackage", {
+        "number": number,
+        "company": company,
+        "name": $("#add-package p").text()
+    }, (data) => {
+        if (data["state"] != "ok")
+            return;
+    });
+
+    $.post($SCRIPT_ROOT + "/express/getExpressState", {
+        "number": number,
+        "company": company
+    }, (data) => {
+        let result = $(data);
+        $("#packages-list").append(result[0]);
+        $("#express-card .col-7 .tab-content").append(result[2]);
+    });
+}
+
 function deletePackage() {
     let curr_item = $(".package-item.active");
     let curr_track = $(".track-detail.active");
